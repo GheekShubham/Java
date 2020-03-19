59d101cc15921d310066d3fa44c0bc4fc2c085f9
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-19 23:21:55 +0530

    Create ArmstrongNumber.java

---
+++ ArmstrongNumber.java
@@ -0,0 +1,39 @@
+/**************************************************************************
+**** BASIC PROGRAM TO EVALUAE WHETHER GIVEN NUMBER IS ARMSTRONG OR NOT ****
+**************************************************************************/
+
+import java.util.Scanner;
+class ArmstrongNumber {
+	
+	public static int count(int number) {
+		int counter = 0;
+		while(number != 0) {
+			counter++;
+			number /= 10;
+		}
+		return counter;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int temp = 0, resultNum = 0, digit = 0, count = 0;
+		
+		System.out.print("Enter number to check whether it is Armstrong or not: ");
+		int num = sc.nextInt();
+		
+		temp = num;
+		count = count(temp);
+		while(temp != 0) {
+			digit = temp % 10;
+			resultNum += Math.pow(digit, count);
+		}
+		
+		if(num == resultNum) {
+			System.out.println(num+" is Armstrong number !!!");
+		}else {
+			System.out.println(num+" is not Armstrong number !!!");
+		}
+		
+		sc.close();
+	}
+}

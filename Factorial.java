e537c9f4b001c8bfd77fc8dfdbcb09e264ec4d5c
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-23 09:24:04 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,26 @@
+/******************
+**** FACTORIAL ****
+******************/
+package forLoop;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number to find factorial: ");
+		int n = sc.nextInt();
+		
+		int result = 1;
+		
+		for(int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		
+		System.out.println("Factorial of "+n+" is "+result+ ".");
+		sc.close();
+	}
+}

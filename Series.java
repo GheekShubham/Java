d870d008de6b724dc13d7881145ced63f29d747d
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-24 09:12:08 +0530

    Create Series.java
    Sum of series (1 + 1/2 + 1/3 + 1/4...)

---
+++ Series.java
@@ -0,0 +1,23 @@
+/*************************************************
+**** Sum of series (1 + 1/2 + 1/3 + 1/4...)   ****
+*************************************************/
+package forLoop;
+
+import java.util.Scanner;
+
+public class Series1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of terms: ");
+		int n = sc.nextInt();
+		
+		float sum = 0.0f;
+		
+		for(float i = 1; i <= n; i++) {
+			sum +=(1.0/i);
+		}
+		System.out.println("Sum of series upto "+n+" terms is "+sum+".");
+		sc.close();
+	}
+}

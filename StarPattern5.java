0a68a599eb17107d5912a732623cc2e771945210
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-24 13:35:40 +0530

    Create StarPattern5.java

---
+++ StarPattern5.java
@@ -0,0 +1,31 @@
+/***********************************************
+****        STAR PATTERN EXAMPLE            ****
+****                        *               ****
+****                      * *               ****
+****                    * * *               ****
+****                  * * * *               ****
+****                * * * * *               ****
+***********************************************/
+package forLoop;
+
+import java.util.Scanner;
+
+public class StarPattern5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number of rows: ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = i; j <= n-1; j++) {
+				System.out.print("  ");
+			}
+			for(int k = 1; k <= i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}

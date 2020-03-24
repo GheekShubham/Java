63b8a137dc05a2a6666ff04fa5915a6497e2c148
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-24 18:24:00 +0530

    Create SumOfDigits.java

---
+++ SumOfDigits.java
@@ -0,0 +1,23 @@
+// Sum of digits of number
+package whileLoop;
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number: ");
+		int num = sc.nextInt();
+		int temp = num;
+		int sum = 0;
+		
+		while(temp != 0) {
+			sum += temp%10;
+			temp /= 10;
+		}
+		System.out.println("Sum of digits of "+num+" is "+sum+".");
+		sc.close();
+	}
+}

07a1b90616066518d82a0ac9fcfdb19256fd8cf7
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-25 16:17:18 +0530

    Create BubbleSort1.java
    I am beginner and always seeking for feedback, suggestions, contributions...

---
+++ Algorithm/BubbleSort1.java
@@ -0,0 +1,31 @@
+package bubbleSort;
+
+public class BubbleSort1 {
+	
+	public void bubbleSort(int[] array) {
+		int len = array.length;
+		int count = len - 1;
+		for(int i = 0; i < len - 1; i++) {
+			for(int j = 0; j < count; j++) {
+				if(array[j] > array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}else {
+					j++;
+				}
+			}
+			count--;
+		}
+		System.out.println("Sorted elements are :");
+		for(int i = 0; i < array.length; i++) {
+			System.out.print(array[i]+" ");
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {9, 8, 7, 6, 5, 4, 3};
+		BubbleSort1 bs = new BubbleSort1();
+		bs.bubbleSort(arr);
+	}
+}

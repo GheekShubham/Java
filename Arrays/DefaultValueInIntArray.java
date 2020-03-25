24ae15ea4a095889160da3830fc072d3a41a5582
Shubham Arora <62293683+GheekShubham@users.noreply.github.com>
2020-03-25 09:39:43 +0530

    Create DefaultValueInIntArray.java

---
+++ Arrays/DefaultValueInIntArray.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class DefaultValueInIntArray {
+
+	public static void main(String[] args) {
+		
+		int[] array = new int[5];
+		
+		for(int i = 0; i < array.length; i++) {
+			System.out.println("Index "+i+" => "+array[i]);
+		}
+	}
+}
